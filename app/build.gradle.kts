10a77a9c3a229124886e3d627bccd8210d210667
Genkaim <cxh2066172407@outlook.com>
2026-09-21 09:09:38 +0800

    Correct release version to v0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 14
-        versionName = "0.4.0"
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
